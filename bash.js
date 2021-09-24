d2aa5d66d0557e31f1b51e89074dceae2f93659a
addislw <addislw@gmail.com>
2021-09-24 15:09:59 -0400

    Program now prints the working directory

---
+++ bash.js
@@ -0,0 +1,23 @@
+process.stdout.write('prompt > ');
+
+
+/*
+process.stdin.on('data', (data) => {
+  const cmd = data.toString().trim();
+
+  process.stdout.write('You typed: ' + cmd); 
+  
+  // process.stdout.write('\nprompt > '); 
+})
+*/
+
+process.stdin.on('data', (data) => {
+  const cmd = data.toString().trim();
+
+  process.stdout.write(process.env[`${cmd.toUpperCase()}`]); 
+  
+  // process.stdout.write('\nprompt > '); 
+})
+
+
+
